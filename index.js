6eae5d61f2fffe49e9289f24968e71f7c1253502
yusuf <yusuf.bz915@gmail.com>
2024-04-03 18:08:25 +0000

    test

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ function createWidget({ _id, chat_widget_id, widget, env }) {
 	const launcherIcon = document.createElement('img');
 	launcherIcon.src = "https://assets.lightfunnels.com/account-206/images_library/e41cd459-4c85-4fb7-a4fd-93ce657e26b9.chat.svg"
 	btnLauncher.appendChild(launcherIcon)
-	btnLauncher.addEventListener("click", toggleChatWidget(widget.style.label.hasLabel));
+	btnLauncher.addEventListener("click", () => toggleChatWidget(widget.style.label.hasLabel));
 	if (widget.style.label.hasLabel) {
 
 		const dialog = document.createElement('div');
