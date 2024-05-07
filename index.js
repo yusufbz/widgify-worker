797bd313ef57af3ac72bbcc2588d284e682a6eaa
yusuf <yusuf.bz915@gmail.com>
2024-05-07 15:24:50 +0100

    font fix

--- index.js
+++ index.js
@@ -14,6 +14,23 @@
 // // Append the Bugsnag script to the document
 // document.head.appendChild(bugsnagScript);
 
+// Add links to the head tag
+const link1 = document.createElement('link');
+link1.rel = 'preconnect';
+link1.href = 'https://fonts.googleapis.com';
+document.head.appendChild(link1);
+
+const link2 = document.createElement('link');
+link2.rel = 'preconnect';
+link2.href = 'https://fonts.gstatic.com';
+link2.crossOrigin = true;
+document.head.appendChild(link2);
+
+const link3 = document.createElement('link');
+link3.href = 'https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap';
+link3.rel = 'stylesheet';
+document.head.appendChild(link3);
+
 async function fetchWidget({ ...props }) {
 	// console.log(props)
 	const response = await fetch(`${props.port}/storefront/getWidget?_id=${props.chat_widget_id}`, {
@@ -181,6 +198,8 @@ window.addEventListener('message', function (event) {
 		if (event.data === 'closeChatWidget') closeChatWidget();
 	}
 });
+
+
 
 document.addEventListener('DOMContentLoaded', async () => {
 	try {
